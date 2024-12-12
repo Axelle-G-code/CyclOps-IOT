14e2c065195f8d8dc61b93c009fb3a927a0e36d0
Axelle-G-code <axelle.groothaert21@imperial.ac.uk>
2024-12-12 01:18:25 +0000

    Update ESP32_sensing.ino

--- Sensing (part 1)/ESP32_sensing.ino
+++ Sensing (part 1)/ESP32_sensing.ino
@@ -12,41 +12,41 @@ ESP8266WiFiMulti wifiMulti;
 #include <InfluxDbCloud.h>
 #include <TinyGPS++.h>
 
-// Custom Ultrasonic Sensor Implementation
+// pins for ultrasonic sensor
 #define TRIGGER_PIN 5
 #define ECHO_PIN 18
 
-// WiFi Credentials
+// WiFi connection details
 #define WIFI_SSID "Pixel_1743"
 #define WIFI_PASSWORD "doudou123"
 
-// InfluxDB Configuration
+// InfluxDB configuration
 #define INFLUXDB_URL "https://us-east-1-1.aws.cloud2.influxdata.com"
 #define INFLUXDB_TOKEN "2jRrp5qcV2fAy-T4c8J8pp9JuP9UKEvQOnq1HPvoFDOQO938RIPfDaXRv1cG_RV56ii6O_AZHOGrC7G94qu9-w=="
 #define INFLUXDB_ORG "eaa5d7ece23e8f9d"
 #define INFLUXDB_BUCKET "IOT data"
 
-// Time zone info
+// time zone
 #define TZ_INFO "UTC0"
 
-// Declare InfluxDB client instance with preconfigured InfluxCloud certificate
+// declare InfluxDB client instance with certificate
 InfluxDBClient client(INFLUXDB_URL, INFLUXDB_ORG, INFLUXDB_BUCKET, INFLUXDB_TOKEN, InfluxDbCloud2CACert);
 
-// Declare Data Points
+// data points declaring
 Point gpsPoint("gps_data");
 Point proximityPoint("proximity_data");
 
-// GPS Setup
+// GPS setup
 TinyGPSPlus gps;
-HardwareSerial gpsSerial(1); // Use UART1 for GPS
+HardwareSerial gpsSerial(1); //  UART1 for GPS
 
-// Function to Setup Ultrasonic Sensor
+// function setting up ultrasonic Sensor
 void setupUltrasonic() {
   pinMode(TRIGGER_PIN, OUTPUT);
   pinMode(ECHO_PIN, INPUT);
 }
 
-// Function to Get Distance from Ultrasonic Sensor
+// function to get distance from ultrasonic sensor
 float getDistance() {
   digitalWrite(TRIGGER_PIN, LOW);
   delayMicroseconds(2);
@@ -59,7 +59,7 @@ float getDistance() {
   return distance;
 }
 
-// Function to Connect to Wi-Fi
+// function to connect to wifi
 void connectToWiFi() {
   wifiMulti.addAP(WIFI_SSID, WIFI_PASSWORD);
   Serial.print("Connecting to Wi-Fi");
@@ -70,7 +70,7 @@ void connectToWiFi() {
   Serial.println("\nWi-Fi connected.");
 }
 
-// Function to Setup InfluxDB
+// function to set up InfluxDB
 void setupInfluxDB() {
   timeSync(TZ_INFO, "pool.ntp.org", "time.nis.gov");
   if (client.validateConnection()) {
@@ -82,7 +82,7 @@ void setupInfluxDB() {
   }
 }
 
-// Function to Publish GPS Data
+// function to publish GPS data
 void publishGPSData(double lat, double lon, double speed) {
   gpsPoint.clearFields();
   gpsPoint.addField("latitude", lat);
@@ -97,7 +97,7 @@ void publishGPSData(double lat, double lon, double speed) {
   }
 }
 
-// Function to Publish Proximity Data
+// function to publish proximity data
 void publishProximityData(float distance) {
   proximityPoint.clearFields();
   proximityPoint.addField("distance", distance);
@@ -110,7 +110,7 @@ void publishProximityData(float distance) {
   }
 }
 
-// Function to Collect and Log Data
+// function to collect and log data
 void collectAndLogData() {
   // Read GPS Data
   double lat = gps.location.lat();
@@ -120,7 +120,7 @@ void collectAndLogData() {
   // Read Ultrasonic Sensor Data
   float distance = getDistance();
 
-  // Log GPS Data to Serial
+  // log gps data to serial
   if (gps.location.isValid()) {
     Serial.print("GPS Data - Latitude: ");
     Serial.print(lat, 6);
@@ -133,12 +133,12 @@ void collectAndLogData() {
     Serial.println("GPS Data - No valid GPS fix.");
   }
 
-  // Log Proximity Sensor Data to Serial
+  // Log proximity sensor data to serial
   Serial.print("Proximity Data - Distance: ");
   Serial.print(distance);
   Serial.println(" cm");
 
-  // Publish Data to InfluxDB
+  // publish data to InfluxDB
   if (gps.location.isValid()) {
     publishGPSData(lat, lon, speed);
   }
@@ -147,12 +147,12 @@ void collectAndLogData() {
 
 void setup() {
   Serial.begin(9600);
-  gpsSerial.begin(9600, SERIAL_8N1, 17, 16); // Initialize GPS (RX=17, TX=16)
-  setupUltrasonic();                         // Initialize Ultrasonic Sensor
-  connectToWiFi();                           // Connect to Wi-Fi
-  setupInfluxDB();                           // Setup InfluxDB
+  gpsSerial.begin(9600, SERIAL_8N1, 17, 16); // initialize GPS (RX=17, TX=16)
+  setupUltrasonic();                         // initialize Ultrasonic Sensor
+  connectToWiFi();                           // connect to WiFi
+  setupInfluxDB();                           // set up InfluxDB
 
-  // Add tags to the data points
+  // adding tags to the data points
   gpsPoint.addTag("device", DEVICE);
   gpsPoint.addTag("SSID", WiFi.SSID());
 
@@ -164,6 +164,6 @@ void loop() {
   while (gpsSerial.available() > 0) {
     gps.encode(gpsSerial.read()); // Parse GPS data
   }
-  collectAndLogData();            // Collect, log, and publish data
+  collectAndLogData();            // collect, log and publish data
   delay(1000);                    // Wait 1 second before next cycle
 }
